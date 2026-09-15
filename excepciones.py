e55d354ef5f00db51f95461a182e52e0b0c62ad5
BoscoGlez <jbgonzalez1311@gmail.com>
2026-09-15 13:34:28 +0200

    Centraliza excepciones propias en modulo excepciones.py

---
+++ excepciones.py
@@ -0,0 +1,12 @@
+class DatosIncompletosError(Exception):
+    pass
+
+
+class CategoriaInvalidaError(Exception):
+    pass
+
+class FechaInvalidaError(Exception):
+     pass
+
+class ImporteInvalidoError(Exception):
+    pass
